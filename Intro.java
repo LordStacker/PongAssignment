61c5663fc145c89463df9b07a8fbf0e5d67cf9a1
lordstacker <nicomatryx@gmail.com>
2022-09-22 14:29:30 +0200

    done

--- Intro.java
+++ Intro.java
@@ -16,10 +16,11 @@ public class Intro extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(950, 600, 1); 
+        Greenfoot.start();
     }
     public void act()
     {
-        if(Greenfoot.isKeyDown("space")){
+        if(Greenfoot.isKeyDown("enter")){
         Greenfoot.setWorld(new Board());
     }
     }
